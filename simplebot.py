0a38df78fc31eface5a96990a3d3a4867d71eefb
Mike Abrahall <mijofa@abrahall.id.au>
2015-02-18 17:50:27 +0000

    Mostly just tidying things up. I completely redid the event loop though.

--- simplebot.py
+++ simplebot.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import multiprocessing
+import pyfirmata
 import time
+
+SPEED = 10
 
 LED_FRONTRIGHT = 0
 LED_BACKRIGHT  = 1
@@ -19,8 +22,12 @@ COLOUR_YELLOW = (255, 200, 0  )
 COLOUR_VIOLET = (255, 0,   200)
 COLOUR_WHITE  = (255, 200, 200)
 
-class leds():
-    _indicator_process = None
+###
+### Controller classes
+###
+
+class led_controller():
+    indicating = False
     leds = [] # Current state of each led
     def __init__(self, board, num_leds):
         self.board = board # The pyfirmata board instance
@@ -47,27 +54,35 @@ class leds():
         self.board.send_sysex(0x71, '{ s }')
 
     def _indicator(self, leds):
+        state = False
+        last_time = 0
         while True:
-            self.set(leds[0], COLOUR_YELLOW)
-            self.set(leds[1], COLOUR_YELLOW)
-            self.push()
-            time.sleep(0.3)
-            self.set(leds[0], COLOUR_BLACK)
-            self.set(leds[1], COLOUR_BLACK)
-            self.push()
-            time.sleep(0.3)
+            if time.time() > last_time + 0.3:
+                if  state == False:
+                    for i in leds:
+                        self.set(i, COLOUR_YELLOW)
+                    state = True
+                elif state == True:
+                    for i in leds:
+                        self.set(i, COLOUR_BLACK)
+                    state = False
+                else:
+                    raise Exception('WTF')
+                self.push()
+                last_time = time.time()
     def indicate(self, leds):
         assert leds == None or (leds[0] < len(self.leds) and leds[0] >= 0 and leds[1] < len(self.leds) and leds[1] >= 0)
-        if self._indicator_process != None:
+        if self.indicating == True:
             self._indicator_process.terminate() # Stop any indicator process currently running
             self.set(self._indicator_process._args[0][0], COLOUR_BLACK) # Reset the state of the LED that process was flashing
             self.set(self._indicator_process._args[0][1], COLOUR_BLACK) # Reset the state of the LED that process was flashing
             self.push()
+            self.indicating = False
         if leds != None:
             self._indicator_process = multiprocessing.Process(target=self._indicator, args=[leds], name='indicator %d %d' % leds)
             self._indicator_process.start()
 
-class move():
+class movement_controller():
     def __init__(self, board, left_pin, right_pin, left_stop=90, right_stop=90):
         self.board       = board
         self.left_stop   = left_stop
@@ -92,7 +107,7 @@ class move():
         self.left_wheel.write(self.left_stop   + speed)
         self.right_wheel.write(self.right_stop + speed)
 
-class dist():
+class distance_sensor():
     ### I spent hours trying to makes sense of how the node.js code did all this before giving up and just copying magic numbers from what the node.js code did.
     # FIXME: Make sense of this.
     callback = None
@@ -111,7 +126,7 @@ class dist():
         self.cm     = duration / 58.0
         self.inches = duration / 148.0
         if self.callback != None:
-            try: self.callback()
+            try: self.callback(self.value, self.cm, self.inches)
             except: print "Callback failed"
     def pulse(self, value = 1, pulseOut = 0, timeout = 1000000): # None of these arguments are used until I make sense of the magic numbers
 #        data = [
@@ -123,44 +138,132 @@ class dist():
         data = [ self.pin, 1, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 15, 0, 66, 0, 64, 0 ]
         self.board.send_sysex(0x74, data)
 
+###
+### Functions for binding to keys
+## The first argument will always be the current gear
+## If the function returns anything other than None then it will be the new gear.
+def accel(gear):
+    if leds.indicating:
+        indicate_stop(gear)
+    if   gear <  0:
+        move.backward(gear*-SPEED)
+    elif gear == 0:
+        pass
+    elif gear >  0:
+        move.forward(gear*SPEED)
+def brake(gear = None):
+    for i in (LEDS_BACK):
+        leds.set(i, COLOUR_RED)
+    leds.push()
+    move.stop()
+def left(gear):
+    if leds.indicating and not prev_state in ('left', 'indicate_left'):
+        indicate_stop(gear)
+    # I'm not sure how intuitive this will be, but if we are reversing spin right instead of left
+    if   gear <  0:
+        move.right(gear*-SPEED)
+    elif gear == 0:
+        pass
+    elif gear >  0:
+        move.left(gear*SPEED)
+def right(gear):
+    if leds.indicating and not prev_state in ('right', 'indicate_right'):
+        indicate_stop(gear)
+    # I'm not sure how intuitive this will be, but if we are reversing spin left instead of right
+    if   gear <  0:
+        move.left(gear*-SPEED)
+    elif gear == 0:
+        pass
+    elif gear >  0:
+        move.right(gear*SPEED)
+def indicate_left(gear = None):
+    leds.indicate(LEDS_LEFT)
+def indicate_right(gear = None):
+    leds.indicate(LEDS_RIGHT)
+def indicate_stop(gear = None):
+    leds.indicate(None)
+def gear_up(gear):
+    if gear == 1:
+        # This should disable reversing rather than just reseting
+        leds.reset()
+    return gear+1
+def gear_down(gear):
+    if gear <= 0:
+        for i in (LEDS_BACK):
+            leds.set(i, COLOUR_WHITE)
+        leds.push()
+    return gear-1
+
+###
+### Initialisations
+###
+
+## Gears
+# <0 = reverse speed = gear*-SPEED
+#  0 = stationary
+# >0 = forward speed = gear*SPEED
+gear = 0
+
+board = pyfirmata.Arduino('/dev/ttyS99')
+# Start an iterator thread so that pyfirmata can handle reading pin values and avoid the serial buffer from filling up.
+it = pyfirmata.util.Iterator(board)
+it.setDaemon(True)
+it.start()
+
+move = movement_controller(board,9,10,93,94) # 93 & 94 are magic numbers, but they're my magic numbers. These should be set in a config file in /boot
+leds = led_controller(board,4)
+dist = distance_sensor(board, 8)
+
 if __name__ == '__main__':
     import sys, tty
-    import pyfirmata
 
     fd = sys.stdin.fileno()
     tty.setcbreak(fd)
 
+    bindings = { # This should probably be configurable, but this is reasonable defaults
+        'w':     accel,
+        's':     brake,
+        'a':     left,
+        'd':     right,
+        'up':    gear_up,
+        'down':  gear_down,
+        'left':  indicate_left,
+        'right': indicate_right,
+    }
 
-    b = pyfirmata.Arduino('/dev/ttyS99')
-    m = move(b,9,10,93,94) # 93 & 94 are magic numbers, but they're my magic numbers. These should be set in a config file in /boot
-    l = leds(b,4)
-
-    b.digital[13].write(1) # Turn on the blink LED to let the user know we are ready to go.
+    prev_state = None
+    board.digital[13].write(1) # Turn on the blink LED to let the user know we are ready to go.
+    def f(value, cm, inches):
+        print "Distance:", cm, "cm"
+    dist.callback = f
     while True:
+        dist.pulse()
         key = sys.stdin.read(1)
-        sys.stdout.write(key)
-        if key    == '1':
-            l.indicate(None)
-        elif key  == 'q':
-            l.indicate(LEDS_LEFT)
-        elif key == 'w':
-            l.indicate(None)
-            m.forward(30)
-        elif key == 'e':
-            l.indicate(LEDS_RIGHT)
-        elif key == 'a':
-            m.left(30)
-        elif key == 's':
-            l.indicate(None)
-            m.backward(30)
-        elif key == 'd':
-            m.right(30)
-        elif key == ' ':
-            m.stop()
-            l.indicate(None)
-        elif key == '\x04': # ^d
+#        sys.stdout.write(key)
+
+        if ord(key) == 27: # Esc
+            key = sys.stdin.read(1)
+            if ord(key) == 91: # I don't actually understand the numbers now, but all the arrow keys had this next
+                key = sys.stdin.read(1)
+                if   ord(key) == 65: # up
+                    key = 'up'
+                elif ord(key) == 66: # down
+                    key = 'down'
+                elif ord(key) == 67: # right
+                    key = 'right'
+                elif ord(key) == 68: # left
+                    key = 'left'
+        if key in bindings.keys():
+            print gear, bindings[key].__name__
+            ret = bindings[key](gear)
+            if ret != None:
+                gear = ret
+            prev_state = bindings[key].__name__
+        elif ord(key) == 4: # ^d
             break
 
-    b.digital[13].write(0)
-    l.indicate(None)
-    m.stop()
+    board.digital[13].write(0)
+    leds.reset()
+    move.stop()
+
+# python -i -c 'import pyfirmata,simplebot ; b = pyfirmata.Arduino("/dev/ttyS99") ; it = pyfirmata.util.Iterator(b) ; it.setDaemon(True) ; it.start()'
