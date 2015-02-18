243893bc3c3bb70b71b7e6d6da7bb4f70fe46df9
Mike Abrahall <mijofa@abrahall.id.au>
2015-02-18 12:16:39 +0000

    Fixed the COLOUR variables. Added distance sensor class.

--- simplebot.py
+++ simplebot.py
@@ -11,21 +11,26 @@ LEDS_RIGHT     = (0,1)
 LEDS_FRONT     = (0,3)
 LEDS_BACK      = (1,2)
 
-class leds():
-    COLOUR_BLACK  = (0,   0,   0  )
-    COLOUR_RED    = (255, 0,   0  )
-    COLOUR_GREEN  = (0,   255, 0  )
-    COLOUR_BLUE   = (0,   0,   255)
-    COLOUR_YELLOW = (255, 200, 0  )
-    COLOUR_VIOLET = (255, 0,   200)
-    COLOUR_WHITE  = (255, 200, 200)
+COLOUR_BLACK  = (0,   0,   0  )
+COLOUR_RED    = (255, 0,   0  )
+COLOUR_GREEN  = (0,   255, 0  )
+COLOUR_BLUE   = (0,   0,   255)
+COLOUR_YELLOW = (255, 200, 0  )
+COLOUR_VIOLET = (255, 0,   200)
+COLOUR_WHITE  = (255, 200, 200)
 
+class leds():
     _indicator_process = None
     leds = [] # Current state of each led
     def __init__(self, board, num_leds):
         self.board = board # The pyfirmata board instance
         for i in range(num_leds):
             self.leds.append(0)
+    def reset(self):
+        self.indicate(None)
+        for i in range(len(self.leds)):
+            self.set(i, COLOUR_BLACK)
+        self.push()
     def set(self, led, colour):
         assert led < len(self.leds) and led >= 0
         assert type(colour) == tuple and len(colour) == 3
@@ -87,6 +92,36 @@ class move():
         self.left_wheel.write(self.left_stop   + speed)
         self.right_wheel.write(self.right_stop + speed)
 
+class dist():
+    ### I spent hours trying to makes sense of how the node.js code did all this before giving up and just copying magic numbers from what the node.js code did.
+    # FIXME: Make sense of this.
+    callback = None
+    def __init__(self, board, pin): # I don't know enough about the firmata code to separate trigpin and echopin
+        self.board = board
+        self.pin   = pin
+        self.board.add_cmd_handler(0x74, self._response)
+    def _response(self, *currentBuffer):
+        ### I don't understand how any of this math works, I copied it from the Johnny-five node.js code and thankfully Python's syntax seems to be close enough that I was able to translate it without actually understanding.
+        durationBuffer = [ (currentBuffer[2] & 0x7F) | ((currentBuffer[3] & 0x7F) << 7), (currentBuffer[4] & 0x7F) | ((currentBuffer[5] & 0x7F) << 7), (currentBuffer[6] & 0x7F) | ((currentBuffer[7] & 0x7F) << 7), (currentBuffer[8] & 0x7F) | ((currentBuffer[9] & 0x7F) << 7) ]
+        duration = (durationBuffer[0] << 24) + (durationBuffer[1] << 16) + (durationBuffer[2] << 8) + (durationBuffer[3])
+
+        # These magic numbers I pulled from the tech specs of the HC-SR04 sensor
+        # I don't think I have any choice but to use magic numbers, although they should probably go in a config file somewhere.
+        self.value  = duration
+        self.cm     = duration / 58.0
+        self.inches = duration / 148.0
+        if self.callback != None:
+            try: self.callback()
+            except: print "Callback failed"
+    def pulse(self, value = 1, pulseOut = 0, timeout = 1000000): # None of these arguments are used until I make sense of the magic numbers
+#        data = [
+#                self.pin,
+#                value,
+#                pulseOut,
+#                timeout,
+#                ]
+        data = [ self.pin, 1, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 15, 0, 66, 0, 64, 0 ]
+        self.board.send_sysex(0x74, data)
 
 if __name__ == '__main__':
     import sys, tty
